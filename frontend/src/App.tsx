247cd15f3e35a7afccbd637d35bfd4abbf9f3558
B K Park <pbk95120@gmail.com>
2023-11-30 23:41:07 +0900

    add: add register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignUpPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   );
 }
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -12,13 +13,16 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="/">
+        사이트명
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -26,17 +30,84 @@ function Copyright(props: any) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
 const SignUpPage = () => {
+  const defaultTheme = createTheme();
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  const [isAlert, setIsAlert] = useState(false);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const email = event.target.value;
+    emailRegex(email);
+  };
+
+  const emailRegex = (email: string) => {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    !emailRegex.test(email) ? setEmailError("Not a valid Email format") : setEmailError("");
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const password = event.target.value;
+    setPassword(event.target.value);
+    passwordRegex(password);
+  };
+
+  const passwordRegex = (password: string) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+    !passwordRegex.test(password)
+      ? setPasswordError("Password must be at least 8 character, contain letters and numbers.")
+      : setPasswordError("");
+  };
+
+  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const confirmPaassword = event.target.value;
+    confirmPasswordRegex(confirmPaassword);
+  };
+
+  const confirmPasswordRegex = (confirmPassword: string) => {
+    confirmPassword !== password ? setConfirmPasswordError("Password doesn't match") : setConfirmPasswordError("");
+  };
+
+  const handleCheckBox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
+    //패스워드 같은지 확인다시한번
+    if (data.get("password") !== data.get("confirmPassword")) {
+      setConfirmPasswordError("Password doesn't match");
+    }
+
+    //체크박스 확인
+    if (!isChecked) {
+      setIsAlert(true);
+    }
+
+    //값 비었는지 확인
+    if (data.get("email") === "") {
+      setEmailError("Not a valid Email format");
+    } else if (data.get("password") === "") {
+      setPasswordError("Password must be at least 8 character, contain letters and numbers.");
+    }
+
+    //입력값 검증 후 폼 전송
+    else if (
+      emailError === "" &&
+      passwordError === "" &&
+      confirmPasswordError === "" &&
+      data.get("password") === data.get("confirmPassword") &&
+      isChecked === true
+    ) {
+      console.log(data + "폼전송");
+      navigate("/signin");
+    }
   };
 
   return (
@@ -55,48 +126,77 @@ const SignUpPage = () => {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Sign up
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                {isAlert && (
+                  <Stack sx={{ width: "100%" }} spacing={2}>
+                    <Alert severity="error">You must agree to the checkbox!</Alert>
+                  </Stack>
+                )}
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  error={emailError !== ""}
+                  onChange={handleEmailChange}
+                />
               </Grid>
+              <p className="text-xs text-rose-600 ml-4">{emailError}</p>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  error={passwordError !== ""}
+                  onChange={handlePasswordChange}
+                />
+              </Grid>
+              <p className="text-xs text-rose-600 ml-4 mt-2">{passwordError}</p>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="confirmPassword"
+                  error={confirmPasswordError !== ""}
+                  onChange={handleConfirmPasswordChange}
+                />
+              </Grid>
+              <p className="text-xs text-rose-600 ml-4 mt-2">{confirmPasswordError}</p>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox color="primary" onChange={handleCheckBox} />}
+                  label="I agree to be a member"
+                />
+              </Grid>
+            </Grid>
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                <Link href="/signin" variant="body2">
+                  Already have an account? Sign in
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
   );
