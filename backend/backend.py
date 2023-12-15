e911c7ee34cf460aa2ba12960a87283c33d2b6aa
Jeon980902 <amico98@naver.com>
2023-12-16 06:07:07 +0900

    update file structure

--- backend/backend.py
+++ backend/backend.py
@@ -1,47 +1,52 @@
+#Import Fastapi
+#for making server
 from fastapi import FastAPI, Form, Request, UploadFile, File
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Optional
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.document_loaders import TextLoader
+
+#Import Langchain
+#for executing queries
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from typing import Optional
 
-
-import uvicorn
-from glob import glob
-import os
-import shutil
-
-
+#Import Etc..
 from pydantic import BaseModel
+from glob import glob
+import uvicorn
+import os
 
-OPEN_API_KEY = "yourkey"
+#This is the key for Using OpenAi
+#Please do not share this key
+OPEN_API_KEY = "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" #You can use your own key
 
+#The Class for Request
+#for defining requesting structure during 'POST'
 class requestData(BaseModel):
-    userId: str
-    password: Optional[str] = None
-    fileName: Optional[str] = None
-    contents: Optional[str] = None
+    userId: str #get User Id as a String, and it must be included in the request
+    password: Optional[str] = None  #get Password as a String, and it is optional
+    fileName: Optional[str] = None  #get File Name as a String, and it is optional
+    contents: Optional[str] = None  #get Contents as a String, and it is optional
 
-origins = [
-    "*",
-]
-
+#Define FastAPI Application
 app = FastAPI()
 templates = Jinja2Templates(directory='./')
 
+#Set All IPs to access backend server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+#Split the text to document loader attributes
 def load_n_split(path):
     loader = TextLoader(path)
     document = loader.load()
@@ -52,6 +57,7 @@ def load_n_split(path):
 
     return docs
 
+#Save Langchain to DB
 def save_as_db(dbPath, dbName, docs):
     path = dbPath + "." + dbName
     index = VectorstoreIndexCreator(
@@ -60,20 +66,23 @@ def save_as_db(dbPath, dbName, docs):
         ).from_documents(docs)
     index.vectorstore.save_local(path)
 
+#Load Langchain to retriver
 def load_from_db(path):
     fdb = FAISS.load_local(path, OpenAIEmbeddings(openai_api_key=OPEN_API_KEY))
     retriever = fdb.as_retriever(search_type='similarity', search_kwargs={"k":2})
 
     return retriever
 
+#Function For Sign Up
 @app.post('/signup')
 def return_signup_stat(request: requestData):
+    #Get All User Directories in server
     for path, dir, files in os.walk("./data/"):
         print(dir)
         for folder_name in dir:
+            #When there exists user's folder
             if folder_name == request.userId:
                 return {"exist":True, "status":False}
-    
     try:
         user_dir = "./data/" + request.userId
         os.mkdir(user_dir)
@@ -84,39 +93,47 @@ def return_signup_stat(request: requestData):
     except:
         return {"exist":False, "status":False}       
 
+#Function For Sign In
 @app.post('/signin')
 def return_signin_stat(request: requestData):
+    #Get All User Directories in server
     for path, dir, files in os.walk("./data/"):
         print(dir)
         for folder_name in dir:
+            #When there exists user's folder
             if folder_name == request.userId:
+                #there has a hidden file for looking up password
                 f = open("./data/" + request.userId + "/." + request.userId,"r")
                 password = f.read()
+                #When the password matches
                 if password == request.password:
                     return {"exist":True, "status":True}
+                #When the password not matches
                 else:
                     return {"exist":True, "status":False}
 
     return {"exist":False, "status":False}
 
+#Function For Listing up Notes
 @app.get('/getNoteList/{userid}')
 def return_note_list(userid:str):
     file_list = os.listdir("./data/" + userid)
-    # file_list_txt = [file for file in file_list if file.endswith(".txt")]
     file_list_txt = [file for file in file_list if not file.startswith(".")]
 
     file_list_txt.sort()
 
     return {"fileList": file_list_txt}
 
+#Function For Getting Contents of Notes
 @app.get('/getNote/{userid}/{filename}')
-def return_note_list(userid:str, filename:str):
+def return_note(userid:str, filename:str):
     f = open("./data/" + userid + "/" + filename,"r")
     contents = f.read()
 
     return {"contents": contents}
 
-@app.post("/uploadText")
+#Function For Uploading Notes
+@app.post("/uploadNote")
 async def upload_file(request: requestData):
     upload_dir = "./data/" + request.userId + "/"
     f = open(upload_dir  + request.fileName,"w+")
@@ -128,6 +145,7 @@ async def upload_file(request: requestData):
 
     return {"status": True, "file": request.fileName}
 
+#Function For Getting Query Answer
 @app.get('/getQa/{userid}/{filename}/{query}')
 def return_query(userid:str, filename:str, query:str):
     path = "./data/" + userid + "/." + filename
--- backend/testing_codes/jquery.js
+++ backend/testing_codes/jquery.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
-
-    // $('.loginButton').click(function(){
-    //     fetch("http://localhost:8080/signin")
-    //     .then((response) => response.json())
-    //     .then((data) => console.log(data));
-    // });
-
+    //FOR LOGIN
     $('.loginButton').click(function(){
         var userid = $('#userid').val();
         var password = $('#password').val();
 
         fetch("http://localhost:8080/signin", {
+            //USE POST METHOD
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
+            //BODY SHOULD CONTAIN USER ID & PASSWORD
             body: JSON.stringify({
                 userId: userid,
                 password: password
@@ -24,15 +20,18 @@ $(document).ready(function(){
             .then((data) => console.log(data));
     });
 
+    //FOR SIGN UP
     $('.signupButton').click(function(){
         var userid = $('#userid').val();
         var password = $('#password').val();
 
+        //USE POST METHOD
         fetch("http://localhost:8080/signup", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
+            //BODY SHOULD CONTAIN USER ID & PASSWORD
             body: JSON.stringify({
                 "userId": userid,
                 "password": password
@@ -42,23 +41,28 @@ $(document).ready(function(){
             .then((data) => console.log(data));
     });
 
+    //FOR GET THE NOTE LIST
     $('.getFileButton').click(function(){
         var userid = $('#userid').val();
 
+        //USE GET METHOD
         fetch("http://localhost:8080/getNoteList/"+userid)
             .then((response) => response.json())
             .then((data) => console.log(data));
     });
 
+    //FOR UPLOADING THE NOTES
     $('.uploadTextButton').click(function(){
         var userid = $('#userid').val();
         var data = $('#data').val();
 
-        fetch("http://localhost:8080/uploadText", {
+        //USE POST METHOD
+        fetch("http://localhost:8080/uploadNote", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
+            //BODY SHOULD CONTAIN USER ID & FILE NAME & CONTENTS
             body: JSON.stringify({
                 "userId": userid,
                 "fileName": Date.now().toString(),
@@ -69,18 +73,22 @@ $(document).ready(function(){
             .then((data) => console.log(data));
     });
 
+    //FOR GETTING QA
     $('.qaButton').click(function(){
         var userid = $('#userid').val();
         var question = $('#data').val();
         fileName = "1702669713498"
+        //USE GET METHOD
         fetch("http://localhost:8080/getQa/"+userid+"/"+fileName+"/"+question)
             .then((response) => response.json())
             .then((data) => $('#qa').text(data.query));
     });
 
+    //FOR GETTING THE NOTE
     $('.readButton').click(function(){
         var userid = $('#userid').val();
         var fileName = $('#data').val();
+        //USE GET METHOD
         fetch("http://localhost:8080/getNote/"+userid+"/"+fileName)
             .then((response) => response.json())
             .then((data) => $('#qa').text(data.contents));
