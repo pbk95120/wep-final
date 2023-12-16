6a82cea2ab9cc5c79410d688ab01b140c4c0bc3e
B K Park <pbk95120@gmail.com>
2023-12-16 11:03:24 +0900

    to mac..

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -28,11 +28,11 @@ export const signin = async (userid: string, password: string) => {
   return response.data;
 };
 
-export const uploadSpeech = async (userid: string, filename: string, file: File) => {
+export const uploadSpeech = async (userid: string, notename: string, file: File) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await axios.post(`http://localhost:8080/uploadSpeech/${userid}/${filename}`, formData, {
+  const response = await axios.post(`http://localhost:8080/uploadSpeech/${userid}/${notename}`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
